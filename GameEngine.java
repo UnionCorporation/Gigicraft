087687a7b9b50e798252c0a10596eae3d46090e1
UnionCorporation <43969388+UnionCorporation@users.noreply.github.com>
2021-12-25 17:04:58 +0100

    (update)

--- GameEngine.java
+++ GameEngine.java
@@ -1,1 +1,35 @@
+public class GameEngine implements Runnable {
 
+    private final Thread gameLoopThread;
+
+    public GameEngine(String windowTitle, int width, int height, boolean vsSync, IGameLogic gameLogic) throws Exception {
+        gameLoopThread = new Thread(this, "GAME_LOOP_THREAD");
+        window = new Window(windowTitle, width, height, vsSync);
+        this.gameLogic = gameLogic;
+    }
+
+    public void start() {
+        gameLoopThread.start();
+    }
+    @Override
+    public void run() {
+    } try {
+        init();
+        gameLoop();
+    } catch (Exception excp) {
+        excp.printStackTrace();
+    }
+}
+
+protected void input() {
+    gameLogic.input(window);
+}
+
+protected void update(float interval) {
+    gameLogic.update(interva);
+}
+
+protected void render() {
+    gameLogic.render(window);
+    window.update();
+}
